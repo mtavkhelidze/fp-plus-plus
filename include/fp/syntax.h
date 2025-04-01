b8d423827ca6afc4a994e82d90900be27dbf3c98
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2025-04-01 21:00:56 +0400

    Add $ operator

--- include/fp/syntax.h
+++ include/fp/syntax.h
@@ -10,16 +10,37 @@
 
 namespace fp {
 
+/**
+ * Function application operator (similar to $ in Haskell).
+ * This operator applies the function `f` to the argument `a`.
+ */
 template <typename A>
-constexpr auto operator<<(A a, std::invocable<A> auto f) {
+constexpr auto operator&=(std::invocable<A> auto f, A a) {
     return f(a);
 }
+
+/**
+ * Function composition operator (similar to . in Haskell).
+ * This operator composes two functions `lhs` and `rhs`, applying `rhs` first
+ * and then `lhs`.
+ */
 template <typename F, typename G>
     requires std::invocable<F, typename std::invoke_result_t<G>::type>
 auto operator<<(F&& lhs, G&& rhs) {
     return [lhs = std::forward<F>(lhs), rhs = std::forward<G>(rhs)](auto a) {
-        return rhs(lhs(a));
+        return lhs(rhs(a));  // Apply rhs first, then lhs
     };
 }
+
+/**
+ * Pipe operator (similar to >>= in Haskell, used for chaining).
+ * This operator allows chaining computations, passing the result of one
+ * computation to the next.
+ */
+template <typename A>
+constexpr auto operator>>=(A a, std::invocable<A> auto f) {
+    return f(a);
+}
+
 }  // namespace fp
 #endif  // __FP_PP_SYNTAX_H__
--- test/syntax.cpp
+++ test/syntax.cpp
@@ -1,46 +1,36 @@
 #include <gtest/gtest.h>
 
 #include <fp/fp>
-#include <functional>
 
 using namespace fp;
 
-TEST(Function, FunctionComposition) {
-    auto addFive = [](const int &x) -> int { return x + 5; };
-    auto multByThree = [](const int &x) -> int { return x * 3; };
-    auto toString = [](const int &x) -> std::string {
-        return std::to_string(x);
-    };
+constexpr auto square = [](int x) { return x * x; };
+constexpr auto hello = [](std::string name) { return "Hello " + name; };
 
-    // const auto one = 10 << addFive << multByThree;
-    // ASSERT_EQ(one, 45) << "Function composition failed";
+TEST(Syntax_Operator_Dollar, works_with_primitive_type) {
+    auto actual = square &= 2;
+    auto expected = 4;
 
-    auto x = 10 << multByThree << addFive << addFive;
-    ASSERT_EQ(x, 40) << "Function composition failed";
-
-    auto y = "Misha" <<
-             [](const std::string &s) -> std::string { return "Hello, " + s; };
-    ASSERT_EQ(y, "Hello, Misha") << "Function composition failed";
+    EXPECT_EQ(actual, expected);
 }
 
-// TEST(Function, ComplexArgumentComposition) {
-//     auto linearTransform =
-//       [](const std::pair<int, int> &p) -> std::pair<int, int> {
-//         return {p.first * 2 + 1, p.second * 3 - 1};
-//     };
+TEST(Syntax_Operator_Dollar, works_with_string_type) {
+    auto actual = hello &= "Name";
+    auto expected = "Hello Name";
 
-//     auto length = [](const std::pair<int, int> &p) -> double {
-//         return std::sqrt(p.first * p.first + p.second * p.second);
-//     };
+    EXPECT_EQ(actual, expected);
+}
 
-//     const auto transformationAndLength = linearTransform >>= length;
+TEST(Syntax_Operator_Dollar, works_function_returning_value) {
+    auto actual = square &= square(2);
+    auto expected = 16;
 
-//     std::pair<int, int> input = {3, 4};
-//     double result = transformationAndLength(input);
+    EXPECT_EQ(actual, expected);
+}
 
-//     ASSERT_NEAR(
-//       result,
-//       std::sqrt((3 * 2 + 1) * (3 * 2 + 1) + (4 * 3 - 1) * (4 * 3 - 1)),
-//       1e-6
-//     );
-// }
+TEST(Syntax_Operator_Dollar, works_multiple_function_application) {
+    auto actual = square &= square &= square(2);
+    auto expected = 16 * 16;
+
+    EXPECT_EQ(actual, expected);
+}
