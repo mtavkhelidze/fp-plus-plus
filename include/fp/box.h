9ea407b5e7347c94fdee59de92e95da0cb4126c6
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2025-04-09 15:36:13 +0400

    Finish Box construction

--- include/fp/box.h
+++ include/fp/box.h
@@ -1,10 +1,11 @@
-#include <iostream>
 #ifndef FP_PLUS_PLUS_INCLUDED_FROM_FP_FP
 #error "This file must be included from <fp/fp.h>"
 #endif  // FP_PLUS_PLUS_INCLUDED_FROM_FP_FP
 #ifndef FP_BOX_H
 // NOLINTNEXTLINE:llvm-header-guard
 #define FP_BOX_H
+
+#include <memory>
 
 #include "defs.h"
 
@@ -30,23 +31,15 @@ struct Box {
     // Concrete (includes things like A*)
     explicit Box(const T& t)
         requires(!std::is_pointer_v<T> && !std::is_null_pointer_v<T>)
-        : data{std::make_unique<T>(t)} {
-        std::cerr << ">>> concrete: " << typeid(T).name() << "\n";
-    }
+        : data{std::make_unique<T>(t)} {}
     // Shared
-    explicit Box(std::shared_ptr<T> ptr) : data(std::make_unique<T>(*ptr)) {
-        std::cerr << ">>> shared: " << typeid(T).name() << "\n";
-    }
+    explicit Box(std::shared_ptr<T> ptr) : data(std::make_unique<T>(*ptr)) {}
     // Raw pointer
     explicit Box(T ptr)
         requires(std::is_pointer_v<T>)
-        : data{std::make_unique<T>(ptr)} {
-        std::cerr << ">>> raw pointer: " << typeid(T).name() << "\n";
-    }
+        : data{std::make_unique<T>(ptr)} {}
     // unique
-    explicit Box(std::unique_ptr<T>& ptr) : data(std::move(ptr)) {
-        std::cerr << ">>> unique: " << typeid(T).name() << "\n";
-    }
+    explicit Box(std::unique_ptr<T>& ptr) : data(std::move(ptr)) {}
     // Varargs
     explicit Box(const T&& t, Ts&&... ts)
         requires(sizeof...(Ts) > 0)
@@ -54,22 +47,17 @@ struct Box {
         constexpr size_t size = sizeof...(Ts) + 1;
         auto xs = std::array<T, size>{std::move(t), std::move(ts)...};
         data = std::make_unique<T*>(std::move(xs.data()));
-        std::cerr << ">>> varargs: " << typeid(T).name() << "\n";
     }
     // nullptr
     explicit Box(T /* np */)
         requires std::is_null_pointer_v<T>
-        : data{std::make_unique<T>(nullptr)} {
-        std::cerr << ">>> nullptr: " << typeid(T).name() << "\n";
-    }
+        : data{std::make_unique<T>(nullptr)} {}
     // Move-only
     explicit Box(T&& t)
         requires(
           std::is_move_constructible_v<T> && !std::is_copy_constructible_v<T>
         )
-        : data{std::make_unique<T>(std::move(t))} {
-        std::cerr << ">>> move-only: " << typeid(T).name() << "\n";
-    }
+        : data{std::make_unique<T>(std::move(t))} {}
     // --- Other constructors
     ~Box() = default;
     auto operator=(Box&&) -> Box& = default;
