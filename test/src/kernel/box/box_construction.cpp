778307aa020541a187855181afd2695be6aca71c
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2025-04-21 12:44:13 +0400

    Fix clang-tidy complaints

--- test/src/kernel/box/box_construction.cpp
+++ test/src/kernel/box/box_construction.cpp
@@ -10,16 +10,24 @@
 #include <string>
 #include <type_traits>
 #include <unordered_set>
+#include <utility>
 #include <vector>
+#include <variant>
+#include <functional>
+#include <optional>
+#include <set>
+#include <unordered_map>
 
-// NOLINTBEGIN:(cppcoreguidelines-avoid-magic-numbers)
+
+// NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers, hicpp-no-array-decay)
+
 using ::testing::Test;
 using namespace fp;
 using namespace fp::internal::box;
 
 TEST(Box_Construction, box_with_nothing_in_it) {
     // Nothing type (std::monostate)
-    Box const box;
+    [[maybe_unused]] Box const box;
     static_assert(std::is_same_v<Box<Nothing>, std::decay_t<decltype(box)>>);
 }
 
@@ -40,7 +48,7 @@ TEST(Box_Construction, array_of_pointers) {
 TEST(Box_Construction, const_lvalue_ref) {
     // const lvalue ref
     const int x = 42;
-    auto box = Box(x);
+    [[maybe_unused]] auto box = Box(x);
     static_assert(std::is_same_v<Box<int>, decltype(box)>);
 }
 
@@ -51,7 +59,7 @@ TEST(Box_Construction, copy_only_type) {
         CopyOnly(const CopyOnly&) = default;
         CopyOnly(CopyOnly&&) = delete;
     };
-    auto box = Box(CopyOnly{});
+    [[maybe_unused]] auto box = Box(CopyOnly{});
     static_assert(std::is_same_v<Box<CopyOnly>, decltype(box)>);
 }
 
@@ -59,20 +67,20 @@ TEST(Box_Construction, function_pointer) {
     // function pointer
     using FuncType = void (*)();
     FuncType func = nullptr;
-    auto box = Box(func);
+    [[maybe_unused]] auto box = Box(func);
     static_assert(std::is_same_v<Box<FuncType>, decltype(box)>);
 }
 
 TEST(Box_Construction, lambda_expression) {
     // lambda expression
     auto lambda = []() { return 42; };
-    auto box = Box(lambda);
+    [[maybe_unused]] auto box = Box(lambda);
     static_assert(std::is_same_v<Box<decltype(lambda)>, decltype(box)>);
 }
 
 TEST(Box_Construction, literal_parameter_pack) {
     // literal parameter pack
-    auto box = Box{1, 2, 3};
+    [[maybe_unused]] auto box = Box{1, 2, 3};
     static_assert(std::is_same_v<Box<int, int, int>, decltype(box)>);
 }
 
@@ -82,66 +90,67 @@ TEST(Box_Construction, move_only_rvalue_and_smart_ptr) {
         MoveOnly() = default;
         MoveOnly(MoveOnly&&) = default;
         MoveOnly(const MoveOnly&) = delete;
+        ~MoveOnly() = default;
     };
-    auto box1 = Box(MoveOnly{});
+    [[maybe_unused]] auto box1 = Box(MoveOnly{});
     static_assert(std::is_same_v<Box<MoveOnly>, decltype(box1)>);
 
-    auto box2 = Box{std::make_unique<int>(5)};
+    [[maybe_unused]] auto box2 = Box{std::make_unique<int>(5)};
     static_assert(std::is_same_v<Box<std::unique_ptr<int>>, decltype(box2)>);
 }
 
 TEST(Box_Construction, nullptr_type) {
     // nullptr_t
-    auto box = Box(nullptr);
+    [[maybe_unused]] auto box = Box(nullptr);
     static_assert(std::is_same_v<Box<Nothing>, decltype(box)>);
 }
 
 TEST(Box_Construction, rvalue) {
     // rvalue
-    auto box1 = Box(42);
+    [[maybe_unused]] auto box1 = Box(42);
     static_assert(std::is_same_v<Box<int>, decltype(box1)>);
 
     int const x = 42;
-    auto box2 = Box(x);
+    [[maybe_unused]] auto box2 = Box(x);
     static_assert(std::is_same_v<Box<int>, decltype(box2)>);
 }
 
 TEST(Box_Construction, shared_ptr) {
     // shared_ptr and rvalue shared_ptr
     auto ptr = std::make_shared<std::string>("hello");
-    auto box1 = Box(ptr);
+    [[maybe_unused]] auto box1 = Box(ptr);
     static_assert(std::is_same_v<Box<std::string>, decltype(box1)>);
 
     struct MyType {};
-    auto box2 = Box(std::make_shared<MyType>());
+    [[maybe_unused]] auto box2 = Box(std::make_shared<MyType>());
     static_assert(std::is_same_v<Box<MyType>, decltype(box2)>);
 }
 
 TEST(Box_Construction, std_array) {
     // std::array*
     std::array<int, 3> xs = {1, 2, 3};
-    auto box = Box(xs.data());
+    [[maybe_unused]] auto box = Box(xs.data());
     static_assert(std::is_same_v<Box<int*>, decltype(box)>);
 }
 
 TEST(Box_Construction, std_deque) {
     // std::deque as a value
     std::deque<int> const deq = {1, 2, 3};
-    auto box = Box(deq);
+    [[maybe_unused]] auto box = Box(deq);
     static_assert(std::is_same_v<Box<std::deque<int>>, decltype(box)>);
 }
 
 TEST(Box_Construction, std_function) {
     // std::function
     std::function<int(int)> const func = [](int x) { return x * 2; };
-    auto box = Box(func);
+    [[maybe_unused]] auto box = Box(func);
     static_assert(std::is_same_v<Box<std::function<int(int)>>, decltype(box)>);
 }
 
 TEST(Box_Construction, std_list) {
     // std::list as a value
     std::list<int> const lst = {1, 2, 3};
-    auto box = Box(lst);
+    [[maybe_unused]] auto box = Box(lst);
     static_assert(std::is_same_v<Box<std::list<int>>, decltype(box)>);
 }
 
@@ -152,7 +161,7 @@ TEST(Box_Construction, std_map) {
       {2, "two"  },
       {3, "three"}
     };
-    auto box = Box(mp);
+    [[maybe_unused]] auto box = Box(mp);
     static_assert(std::is_same_v<Box<std::map<int, std::string>>, decltype(box)>
     );
 }
@@ -160,14 +169,14 @@ TEST(Box_Construction, std_map) {
 TEST(Box_Construction, std_optional) {
     // std::optional
     std::optional<int> const opt = 42;
-    auto box = Box(opt);
+    [[maybe_unused]] auto box = Box(opt);
     static_assert(std::is_same_v<Box<std::optional<int>>, decltype(box)>);
 }
 
 TEST(Box_Construction, std_set) {
     // std::set as a value
     std::set<int> const st = {1, 2, 3};
-    auto box = Box(st);
+    [[maybe_unused]] auto box = Box(st);
     static_assert(std::is_same_v<Box<std::set<int>>, decltype(box)>);
 }
 
@@ -178,7 +187,7 @@ TEST(Box_Construction, std_unordered_map) {
       {2, "two"  },
       {3, "three"}
     };
-    auto box = Box(umap);
+    [[maybe_unused]] auto box = Box(umap);
     static_assert(
       std::is_same_v<Box<std::unordered_map<int, std::string>>, decltype(box)>
     );
@@ -186,34 +195,62 @@ TEST(Box_Construction, std_unordered_map) {
 TEST(Box_Construction, std_unordered_set) {
     // std::unordered_set as a value
     std::unordered_set<int> const ust = {1, 2, 3};
-    auto box = Box(ust);
+    [[maybe_unused]] auto box = Box(ust);
     static_assert(std::is_same_v<Box<std::unordered_set<int>>, decltype(box)>);
 }
 
 TEST(Box_Construction, std_vector) {
     // std::vector as a value
     std::vector<int> vec = {1, 2, 3};  // NOLINT
-    auto box = Box(vec);
+    [[maybe_unused]] auto box = Box(vec);
     static_assert(std::is_same_v<Box<std::vector<int>>, decltype(box)>);
 }
 
 TEST(Box_Construction, string_literal) {
     // string literal
-    auto box = Box("hello");
+    [[maybe_unused]] auto box = Box("hello");
     static_assert(std::is_same_v<Box<std::string>, decltype(box)>);
 }
 
 TEST(Box_Construction, unique_ptr) {
     // unique_ptr and rvalue unique_ptr
     auto ptr = std::make_unique<const char*>("hello");
-    auto box1 = Box(ptr);
+    [[maybe_unused]] auto box1 = Box(ptr);
     static_assert(std::is_same_v<Box<const char*>, decltype(box1)>);
 
-    auto box2 = Box(std::make_unique<const char*>("hello"));
+    [[maybe_unused]] auto box2 = Box(std::make_unique<const char*>("hello"));
     static_assert(
       std::is_same_v<
         Box<std::unique_ptr<const char*, std::default_delete<const char*>>>,
         decltype(box2)>
     );
 }
-// NOLINTEND
+
+TEST(Box_Construction, forwarding_reference_resolution) {
+    auto makeBox = [](auto&& x) { return Box(std::forward<decltype(x)>(x)); };
+
+    int i = 5;
+    [[maybe_unused]] auto box1 = makeBox(i);  // lvalue
+    static_assert(std::is_same_v<decltype(box1), Box<int>>);
+
+    [[maybe_unused]] auto box2 = makeBox(10);  // rvalue
+    static_assert(std::is_same_v<decltype(box2), Box<int>>);
+
+    auto const ci = 42;
+    [[maybe_unused]] auto box3 = makeBox(ci);  // const lvalue
+    static_assert(std::is_same_v<decltype(box3), Box<int>>);
+}
+
+TEST(Box_Construction, reference_collapsing_behavior) {
+    int x = 42;
+    int& ref = x;
+    int&& rref = 42;
+
+    [[maybe_unused]] auto box1 = Box(ref);
+    static_assert(std::is_same_v<Box<int>, decltype(box1)>);
+
+    [[maybe_unused]] auto box2 = Box(std::move(rref));
+    static_assert(std::is_same_v<Box<int>, decltype(box2)>);
+}
+
+// NOLINTEND(cppcoreguidelines-avoid-magic-numbers, hicpp-no-array-decay)
--- test/src/kernel/box/box_get.cpp
+++ test/src/kernel/box/box_get.cpp
@@ -2,13 +2,46 @@
 #include <gtest/gtest.h>
 
 #include <memory>
+#include <utility>
 
 using namespace fp;
 using namespace fp::internal::box;
 
+inline constexpr int forty_two = 42;
+
 TEST(Box_getOrNull, default_typed_box) {
     Box<int> const box;
     EXPECT_EQ(box.getOrNull(), nullptr);
+}
+
+TEST(Box_getOrNull, move_semantics) {
+    Box<int> box1(forty_two);
+    const Box<int> box2(std::move(box1));
+    EXPECT_NE(box2.getOrNull(), nullptr);
+    EXPECT_EQ(*box2.getOrNull(), forty_two);
+}
+
+TEST(Box_getOrNull, raw_pointer_stack_warning_documentation_only) {
+    // NOTE: This is a documentation-only test.
+    // Do NOT use pointers to stack memory with Box.
+    // Box assumes ownership via shared_ptr which is unsafe for stack-allocated memory.
+    SUCCEED();
+}
+
+struct Complex {
+    int a; // NOLINT
+    std::string b; // NOLINT
+
+    auto operator==(const Complex& other) const -> bool {
+        return a == other.a && b == other.b;
+    }
+} __attribute__((aligned(32))); // NOLINT
+
+TEST(Box_getOrNull, complex_type) {
+    const Complex c{10, "hello"};
+    const Box<Complex> box(c);
+    ASSERT_NE(box.getOrNull(), nullptr);
+    EXPECT_EQ(*box.getOrNull(), c);
 }
 TEST(Box_getOrNull, default_box) {
     Box<Nothing> const box = Box();
@@ -21,7 +54,7 @@ TEST(Box_getOrNull, empty_box) {
 }
 
 TEST(Box_getOrNull, with_value) {
-    int const value = 42;
+    int const value = forty_two; // NOLINT
     Box<int> const box(value);
     const auto *result = box.getOrNull();
     ASSERT_NE(result, nullptr);
@@ -29,7 +62,7 @@ TEST(Box_getOrNull, with_value) {
 }
 
 TEST(Box_getOrNull, with_shared_pointer) {
-    std::shared_ptr<int> const sharedPtr = std::make_shared<int>(42);
+    std::shared_ptr<int> const sharedPtr = std::make_shared<int>(forty_two);
     auto box = Box(sharedPtr);
     const auto *result = box.getOrNull();
     ASSERT_NE(result, nullptr);
@@ -37,7 +70,7 @@ TEST(Box_getOrNull, with_shared_pointer) {
 }
 
 TEST(Box_getOrNull, with_raw_pointer) {
-    int value = 42;  // NOLINT:(cppcoreguidelines-avoid-magic-numbers)
+    int value = forty_two;  // NOLINT:(cppcoreguidelines-avoid-magic-numbers)
     Box<int *> const box(&value);
     const auto *result = box.getOrNull();
     ASSERT_NE(result, nullptr);
