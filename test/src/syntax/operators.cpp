ea48e950dd38af864e4104ae88a53d7f358410ed
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2026-06-09 22:54:56 +0400

    Update vcpkg

--- test/src/syntax/operators.cpp
+++ test/src/syntax/operators.cpp
@@ -8,37 +8,36 @@
 #include <gtest/gtest.h>
 
 #include <cstddef>
-#include <string>
 
 using namespace fp;
 using namespace fp::syntax;
 using namespace fp::test;
 
-// &=
+// op &=
 TEST(Syntax_Operators_Dollar, usage) {
     auto applied = square &= 10;
     auto expected = square(10);
     ASSERT_EQ(applied, expected);
 }
-// |
+// op |
 TEST(Syntax_Operators_Pipe, usage) {
     auto piped = square | triple | halve;
     auto expected = pipe(square, triple, halve);
     ASSERT_EQ(piped(42), expected(42));
 }
-// |
+// op |
 TEST(Syntax_Operators_Pipe, usage_with_dollar) {
     auto piped = square | triple | halve &= 42;
     auto expected = pipe(always(42), square, triple, halve)(whatever);
     ASSERT_EQ(piped, expected);
 }
-// *
+// op *
 TEST(Syntax_Operators_Compose, usage) {
     auto composed = halve * triple * square;
     auto expected = compose(halve, triple, square);
     ASSERT_EQ(composed(42), expected(42));
 }
-// *
+// op *
 TEST(Syntax_Operators_Compose, usage_with_dollar) {
     auto result = halve* triple* square &= 42;
     auto expected = compose(halve, triple, square)(42);
